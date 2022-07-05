e054e057d2490b13e5a71609c0d46d346e07f385
iamusayev <iamusayev1337@gmail.com>
2022-07-05 15:57:12 +0400

    Added User Criteria

---
+++ src/main/java/com/example/msazxazarauth/model/criteria/UserCriteria.java
@@ -0,0 +1,12 @@
+package com.example.msazxazarauth.model.criteria;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class UserCriteria {
+
+    private String name;
+
+}
