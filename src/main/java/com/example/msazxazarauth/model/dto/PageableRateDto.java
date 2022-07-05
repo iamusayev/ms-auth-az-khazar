0343fb48f90e6bd79a4bc6165f6f7e23be07348d
iamusayev <iamusayev1337@gmail.com>
2022-07-05 15:58:24 +0400

    Added PageableRateDto

---
+++ src/main/java/com/example/msazxazarauth/model/dto/PageableRateDto.java
@@ -0,0 +1,19 @@
+package com.example.msazxazarauth.model.dto;
+
+import com.example.msazxazarauth.mapper.RoleResponseDto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class PageableRateDto <T>{
+
+    private List<RoleResponseDto> rates;
+    private int lastPageNumber;
+    private boolean hasNextPage;
+
+}
