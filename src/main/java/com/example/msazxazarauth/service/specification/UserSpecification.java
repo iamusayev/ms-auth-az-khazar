8d00c79c06226b355a5f421a7da6b4928314b493
iamusayev <iamusayev1337@gmail.com>
2022-07-05 16:01:35 +0400

    Added UserSpecification

---
+++ src/main/java/com/example/msazxazarauth/service/specification/UserSpecification.java
@@ -0,0 +1,37 @@
+package com.example.msazxazarauth.service.specification;
+
+
+import com.example.msazxazarauth.model.criteria.UserCriteria;
+import org.springframework.data.jpa.domain.Specification;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class UserSpecification implements Specification {
+
+
+    private UserCriteria userCriteria;
+
+    private static final String USERNAME = "username";
+
+    public UserSpecification(UserCriteria userCriteria) {
+        this.userCriteria = userCriteria;
+    }
+
+    @Override
+    public Predicate toPredicate(Root root, CriteriaQuery query, CriteriaBuilder criteriaBuilder) {
+        List<Predicate> predicates = new ArrayList<>();
+        if (userCriteria != null) {
+            if (userCriteria.getName() != null) {
+                predicates.add(criteriaBuilder.like(root.get(USERNAME),
+                        "%"+userCriteria.getName()+"%"));
+            }
+        }
+        return criteriaBuilder.and(predicates.toArray(new Predicate[predicates.size()]));
+    }
+}
