94331a14ea187c177f9f06785296e9a8cca19006
iamusayev <iamusayev1337@gmail.com>
2022-07-05 15:54:38 +0400

    Added Role Entity

---
+++ src/main/java/com/example/msazxazarauth/dao/entity/RoleEntity.java
@@ -0,0 +1,57 @@
+package com.example.msazxazarauth.dao.entity;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.List;
+import java.util.Objects;
+
+import static javax.persistence.GenerationType.AUTO;
+
+
+@Entity
+@Table(name = "roles")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class RoleEntity {
+
+    @Id
+    @GeneratedValue(strategy = AUTO)
+    private Long id;
+    private String name;
+
+    @Column(name = "distinguished_name")
+    private String distinguishedName;
+
+    @Column(name = "is_static")
+    private String isStatic;
+
+    private String description;
+
+    @ManyToMany(cascade = CascadeType.PERSIST)
+    @JoinTable(
+            name = "users_roles",
+            joinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id")
+    )
+    @JsonBackReference
+    private List<UserEntity> users;
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RoleEntity that = (RoleEntity) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
