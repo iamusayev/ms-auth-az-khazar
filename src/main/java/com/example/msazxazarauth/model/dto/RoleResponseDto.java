78463826832288861a14a2a5fbbe9a48f872a5eb
iamusayev <iamusayev1337@gmail.com>
2022-07-05 15:58:52 +0400

    Added RoleResponseDto

---
+++ src/main/java/com/example/msazxazarauth/model/dto/RoleResponseDto.java
@@ -0,0 +1,17 @@
+package com.example.msazxazarauth.model.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@Builder
+public class RoleResponseDto {
+
+    private String name;
+    private String isStatic;
+    private String description;
+
+}
