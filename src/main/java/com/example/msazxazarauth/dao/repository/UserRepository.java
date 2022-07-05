bb89b748bd61d484eaa96de5337807e62048a0d2
iamusayev <iamusayev1337@gmail.com>
2022-07-05 15:55:30 +0400

    Added User Repository

---
+++ src/main/java/com/example/msazxazarauth/dao/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.example.msazxazarauth.dao.repository;
+
+import com.example.msazxazarauth.dao.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<UserEntity, Long>, JpaSpecificationExecutor<UserEntity> {
+
+    Optional<UserEntity> findByUsername(String username);
+
+}
