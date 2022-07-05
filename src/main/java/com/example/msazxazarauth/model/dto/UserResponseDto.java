6564f6f2b87d1a1862862879f6022136f735ce95
iamusayev <iamusayev1337@gmail.com>
2022-07-05 15:59:04 +0400

    Added UserResponseDto

---
+++ src/main/java/com/example/msazxazarauth/model/dto/UserResponseDto.java
@@ -0,0 +1,13 @@
+package com.example.msazxazarauth.model.dto;
+
+import liquibase.pro.packaged.B;
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@Builder
+public class UserResponseDto {
+
+    private String name;
+
+}
