aceacb2ee20b8e2de96c00e84c238ffccbd4c612
iamusayev <iamusayev1337@gmail.com>
2022-07-05 15:56:59 +0400

    Added Role Criteria

---
+++ src/main/java/com/example/msazxazarauth/model/criteria/RoleCriteria.java
@@ -0,0 +1,15 @@
+package com.example.msazxazarauth.model.criteria;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+//TODO
+@Data
+@AllArgsConstructor
+public class RoleCriteria {
+
+    private String name;
+    private String isStatic;
+    private String description;
+
+}
