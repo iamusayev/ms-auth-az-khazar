f56e43419385f90b6590caa2f51472bd22f15e7d
iamusayev <iamusayev1337@gmail.com>
2022-07-05 16:02:23 +0400

    Added UserService

---
+++ src/main/java/com/example/msazxazarauth/service/UserService.java
@@ -0,0 +1,110 @@
+package com.example.msazxazarauth.service;
+
+import com.example.msazxazarauth.dao.entity.UserEntity;
+import com.example.msazxazarauth.dao.repository.UserRepository;
+import com.example.msazxazarauth.mapper.UserMapper;
+import com.example.msazxazarauth.model.criteria.PageCriteria;
+import com.example.msazxazarauth.model.criteria.UserCriteria;
+import com.example.msazxazarauth.service.specification.UserSpecification;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class UserService implements UserDetailsService {
+
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        log.info("ActionLog.loadUserByUsername.start: username {} ", username);
+        var user = userRepository.findByUsername(username).orElseThrow(RuntimeException::new);
+        if (user == null) {
+            log.error("User not found ind the database");
+            throw new UsernameNotFoundException("User not found in the database");
+        } else {
+            log.info("User found in the database: {}", username);
+            Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+            user.getRoles().forEach(role -> {
+                authorities.add(new SimpleGrantedAuthority(role.getName()));
+            });
+            log.info("ActionLog.loadUserByUsername.success: username {} ", username);
+            return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), authorities);
+        }
+    }
+
+    public void saveUser(CreateUserDto userDto) {
+        log.info("ActionLog.saveUser.start");
+
+        var userEntity = UserMapper.mapCreateDtoToEntity(userDto);
+        userRepository.save(userEntity);
+
+        log.info("ActionLog.saveUser.end");
+
+    }
+
+
+    public void updateUserPassword(Long id, String password) {
+        log.info("ActionLog.updateUserPassword.start id :{}", id);
+
+        var encryptedPassword = passwordEncoder.encode(password);
+        var userEntity = fetchUserIfExist(id);
+        userEntity.setPassword(encryptedPassword);
+        userRepository.save(userEntity);
+
+        log.info("ActionLog.updateUserPassword.end id :{}", id);
+    }
+
+
+    public PageableUserDto getUsers(UserCriteria userCriteria, PageCriteria pageCriteria) {
+
+        int pageNumber = pageCriteria.getPage();
+        int count = pageCriteria.getCount();
+
+        var pageRequest = PageRequest.of(pageNumber, count);
+        var specification = new UserSpecification(userCriteria);
+
+        Page<UserEntity> userPage = userRepository.findAll(specification, pageRequest);
+        List<UserEntity> roles = userPage.getContent();
+
+        int userPageCounts = userPage.getTotalPages();
+
+
+        if (userPageCounts != 0) {
+            userPageCounts -= 1;
+        }
+
+
+        return PageableUserDto.builder()
+                .users(UserMapper.mapEntitiesToListResponseDto(roles))
+                .lastPageNumber(userPageCounts)
+                .hasNextPage(userPage.hasNext())
+                .build();
+
+
+    }
+
+
+    //TODO NOT FOUND EXCEPTION
+    private UserEntity fetchUserIfExist(Long id) {
+        return userRepository.findById(id).orElseThrow();
+    }
+
+
+}
