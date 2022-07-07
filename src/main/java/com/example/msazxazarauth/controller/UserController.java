48feb17f66bf55e008aacac4f0fbd3d3bbb8b5f3
iamusayev <iamusayev1337@gmail.com>
2022-07-07 10:22:39 +0400

    added UserController

--- src/main/java/com/example/msazxazarauth/controller/UserController.java
+++ src/main/java/com/example/msazxazarauth/controller/UserController.java
@@ -2,12 +2,14 @@ package com.example.msazxazarauth.controller;
 
 import com.example.msazxazarauth.model.criteria.PageCriteria;
 import com.example.msazxazarauth.model.criteria.UserCriteria;
+import com.example.msazxazarauth.service.CreateUserDto;
 import com.example.msazxazarauth.service.PageableUserDto;
 import com.example.msazxazarauth.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/v1/users")
@@ -15,6 +17,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final UserService userService;
+
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void save(@RequestBody @Valid CreateUserDto dto) {
+        userService.saveUser(dto);
+    }
+
+    @PatchMapping("/{id}")
+    public void updateUserPassword(@PathVariable Long id, @RequestBody String password) {
+        userService.updateUserPassword(id, password);
+    }
+
 
     @GetMapping
     public PageableUserDto getUsers(UserCriteria userCriteria, PageCriteria pageCriteria) {
