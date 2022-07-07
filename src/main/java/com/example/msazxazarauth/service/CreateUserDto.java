55d00e5f78d68d73ef9ea34f28edb27e1f9f8f57
iamusayev <iamusayev1337@gmail.com>
2022-07-07 18:33:35 +0400

    added CreateUserDto

--- src/main/java/com/example/msazxazarauth/service/CreateUserDto.java
+++ src/main/java/com/example/msazxazarauth/service/CreateUserDto.java
@@ -1,30 +1,27 @@
 package com.example.msazxazarauth.service;
 
 import com.example.msazxazarauth.mapper.CreateRoleDto;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.Value;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.List;
 
-@Getter
+@Value
 public class CreateUserDto {
 
 
     @NotNull
     @Size(min = 6, max = 16)
-    private String username;
+    String username;
 
     @NotNull
     @Size(min = 6, max = 16)
-    private String password;
+    String password;
 
     @NotEmpty
     @NotNull
-    private List<CreateRoleDto> roles;
+    List<CreateRoleDto> roles;
 
 }
