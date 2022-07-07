1b964463b3ae58b4426b7170d308d2d0763a11ab
iamusayev <iamusayev1337@gmail.com>
2022-07-07 10:21:10 +0400

    added Create Role Dto

--- src/main/java/com/example/msazxazarauth/mapper/CreateRoleDto.java
+++ src/main/java/com/example/msazxazarauth/mapper/CreateRoleDto.java
@@ -1,13 +1,30 @@
 package com.example.msazxazarauth.mapper;
 
+import lombok.NonNull;
 import lombok.Value;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Value
 public class CreateRoleDto {
 
+    @NotNull
+    @NotEmpty
     String name;
+
+    @NotEmpty
+    @NotNull
     String distinguishedName;
+
+    @NotEmpty
+    @NotNull
     String description;
+
+    @NotEmpty
+    @NotNull
+    @Size(max = 16, min = 2)
     String isStatic;
 
 }
