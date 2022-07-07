7135c001a425fe299b1955d5f6089a8732b363d2
iamusayev <iamusayev1337@gmail.com>
2022-07-07 10:21:19 +0400

    added Create User Dto

--- src/main/java/com/example/msazxazarauth/service/CreateUserDto.java
+++ src/main/java/com/example/msazxazarauth/service/CreateUserDto.java
@@ -1,15 +1,28 @@
 package com.example.msazxazarauth.service;
 
-import com.example.msazxazarauth.dao.entity.RoleEntity;
-import lombok.Value;
+import com.example.msazxazarauth.mapper.CreateRoleDto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
-@Value
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CreateUserDto {
 
-    String name;
-    String password;
-    List<RoleEntity> roles;
+
+    private String username;
+
+    @NotNull
+    @NotEmpty
+    private String password;
+
+    @NotEmpty
+    @NotNull
+    private List<CreateRoleDto> roles;
 
 }
