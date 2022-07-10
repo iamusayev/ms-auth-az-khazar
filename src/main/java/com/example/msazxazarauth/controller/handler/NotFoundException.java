8f0f66e2ace232477647f3a60e0568d4982747f0
iamusayev <iamusayev1337@gmail.com>
2022-07-10 11:13:47 +0400

    added NotFoundException

---
+++ src/main/java/com/example/msazxazarauth/controller/handler/NotFoundException.java
@@ -0,0 +1,14 @@
+package com.example.msazxazarauth.controller.handler;
+
+import lombok.Getter;
+
+@Getter
+public class NotFoundException extends RuntimeException {
+
+    private final String code;
+
+    public NotFoundException(String message, String code) {
+        super(message);
+        this.code = code;
+    }
+}
