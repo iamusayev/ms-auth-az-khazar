3b2ebb537b44a2c01dce225ccc874c7154386968
iamusayev <iamusayev1337@gmail.com>
2022-07-10 11:32:58 +0400

    added RoleRepository

--- src/main/java/com/example/msazxazarauth/dao/repository/RoleRepository.java
+++ src/main/java/com/example/msazxazarauth/dao/repository/RoleRepository.java
@@ -7,4 +7,5 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 public interface RoleRepository extends JpaRepository<RoleEntity, Long>, JpaSpecificationExecutor<RoleEntity> {
 
+
 }
