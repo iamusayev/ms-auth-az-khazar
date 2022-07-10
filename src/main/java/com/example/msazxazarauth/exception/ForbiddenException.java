00dd287e3bd1fea0795a68ac0a8e25207db3471b
iamusayev <iamusayev1337@gmail.com>
2022-07-10 11:13:38 +0400

    added ForbiddenException

---
+++ src/main/java/com/example/msazxazarauth/exception/ForbiddenException.java
@@ -0,0 +1,13 @@
+package com.example.msazxazarauth.exception;
+
+import org.springframework.security.access.AccessDeniedException;
+
+public class ForbiddenException extends AccessDeniedException {
+
+    private final String code;
+
+    public ForbiddenException(String msg, String code) {
+        super(msg);
+        this.code = code;
+    }
+}
