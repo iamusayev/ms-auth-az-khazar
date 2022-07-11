95eecc3111aa4ba1a39c04298fabcad118beb664
iamusayev <iamusayev1337@gmail.com>
2022-07-11 12:23:17 +0400

    added SimpleAccessDeniedHandler

--- src/main/java/com/example/msazxazarauth/security/handler/SimpleAccessDeniedHandler.java
+++ src/main/java/com/example/msazxazarauth/security/handler/SimpleAccessDeniedHandler.java
@@ -14,6 +14,7 @@ import java.util.HashMap;
 
 public class SimpleAccessDeniedHandler implements AccessDeniedHandler {
 
+
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
 
