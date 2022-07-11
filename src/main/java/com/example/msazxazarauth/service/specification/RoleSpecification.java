6c4b2ff5b48e9a7ce193f0c4299f4c28e67938d5
iamusayev <iamusayev1337@gmail.com>
2022-07-11 12:22:59 +0400

    added RoleSpecification

--- src/main/java/com/example/msazxazarauth/service/specification/RoleSpecification.java
+++ src/main/java/com/example/msazxazarauth/service/specification/RoleSpecification.java
@@ -14,8 +14,6 @@ import java.util.List;
 
 public class RoleSpecification implements Specification<RoleEntity> {
 
-
-
     private static final String NAME = "name";
     private static final String DESCRIPTION = "description";
 
