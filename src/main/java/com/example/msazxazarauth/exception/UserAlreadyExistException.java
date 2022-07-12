4c8ec6e606fc2c72fe15336f0f067f8f0c8678de
iamusayev <iamusayev1337@gmail.com>
2022-07-12 16:28:36 +0400

    added UserAlreadyExistException

--- src/main/java/com/example/msazxazarauth/exception/UserAlreadyExistException.java
+++ src/main/java/com/example/msazxazarauth/exception/UserAlreadyExistException.java
@@ -7,7 +7,6 @@ public class UserAlreadyExistException extends RuntimeException {
 
     private final String code;
 
-
     public UserAlreadyExistException(String message, String code) {
         super(message);
         this.code = code;
