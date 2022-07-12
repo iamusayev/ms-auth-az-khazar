48fe0eb8e35c305cf6cd59158d76f2fb2810bd31
iamusayev <iamusayev1337@gmail.com>
2022-07-12 16:26:49 +0400

    added RoleMapper

--- src/main/java/com/example/msazxazarauth/mapper/RoleMapper.java
+++ src/main/java/com/example/msazxazarauth/mapper/RoleMapper.java
@@ -10,6 +10,7 @@ import java.util.List;
 @Mapper
 public abstract class RoleMapper {
 
+
     private static final RoleMapper INSTANCE = Mappers.getMapper(RoleMapper.class);
 
     public abstract RoleEntity mapCreateDtoToEntity(CreateRoleDto roleDto);
