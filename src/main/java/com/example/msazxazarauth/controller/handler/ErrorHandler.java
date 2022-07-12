1941ea1ce18dbac3baad4c28c858801a543250e9
iamusayev <iamusayev1337@gmail.com>
2022-07-12 16:24:07 +0400

    added Error Handler

--- src/main/java/com/example/msazxazarauth/controller/handler/ErrorHandler.java
+++ src/main/java/com/example/msazxazarauth/controller/handler/ErrorHandler.java
@@ -17,6 +17,7 @@ import static com.example.msazxazarauth.model.constants.ExceptionConstants.*;
 @Slf4j
 public class ErrorHandler {
 
+
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ExceptionDto handle(Exception ex) {
