4e54a81c01d2f54086cf612b7096f3840b6a5fb5
Anar Musayev <anarmusayev@digirella.az>
2022-07-27 20:23:35 +0400

    added ForbiddenException

--- src/main/java/com/example/msazxazarauth/exception/ForbiddenException.java
+++ src/main/java/com/example/msazxazarauth/exception/ForbiddenException.java
@@ -4,7 +4,6 @@ import org.springframework.security.access.AccessDeniedException;
 
 public class ForbiddenException extends AccessDeniedException {
 
-
     private final String code;
 
     public ForbiddenException(String msg, String code) {
