19bea1071e0b1fb820b73af03aa90242cd314460
Anar Musayev <anar.musayev@digirella.az>
2022-07-30 20:23:23 +0400

    added

---
+++ src/test/java/com/example/msazxazarauth/MsAzXazarAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.msazxazarauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAzXazarAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
